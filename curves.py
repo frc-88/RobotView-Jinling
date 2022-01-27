786bdeedfee8518bfd2ef381a0ecb3600b307e35
Jinling-Code <64452028+Jinling-Code@users.noreply.github.com>
2022-01-26 23:36:54 -0500

    Add files via upload

--- curves.py
+++ curves.py
@@ -8,9 +8,9 @@ import numpy as np
 red = [50, 0, 200]
 x1, y1, z1 = red
 red_mod = math.sqrt(x1*x1+y1*y1+z1*z1)
-angle_th = 30
+angle_th = 20
 
-dir = 'C:/FILES/downloads/balls/'
+dir = 'C:/FILES/TJ square/balls'
 
 names = os.listdir(dir)
 names = list(filter(lambda x: '.jpg' in x, names))
@@ -66,17 +66,27 @@ for img_idx, name in enumerate(names):
     h, w = img.shape[:2]
     mask = np.zeros(img.shape[:2]).astype(np.uint8)
     cont_img = np.zeros(img.shape[:2]).astype(np.uint8)
-    cross_img = np.zeros((h*2, w*2)).astype(np.uint8)
+    object_img = np.zeros(img.shape[:2]).astype(np.uint8)
+    arcs_img = np.zeros(img.shape[:2]).astype(np.uint8)
+    cirs_img = np.zeros(img.shape[:2]).astype(np.uint8)
+    ellipses_img = np.zeros(img.shape[:2]).astype(np.uint8)
 
     # matrices processing is much faster than pixel level processing
+    # mask is binarized image, white pixels are red in the original image
     imgr, imgg, imgb = img[:,:,0].astype(np.int32), img[:,:,1].astype(np.int32), img[:,:,2].astype(np.int32)
     img_mod = np.sqrt(imgr*imgr+imgg*imgg+imgb*imgb)
     v1_dot_v2 = x1*imgr+y1*imgg+z1*imgb
     cos_img = v1_dot_v2/(red_mod*img_mod+1)
     mask = ((cos_img>math.cos(np.pi*angle_th/180))*255).astype(np.uint8)
 
-    contours = getContours(mask, 128)
-    objects = [contour for contour in contours if len(contour) > 35*4]
+    contours = getContours(mask, 128) # Get contours of all red objects
+    for contour in contours:
+        for x, y in contour:
+            cont_img[y,x] = 255
+    objects = [contour for contour in contours if len(contour) > 35*4]  # Ignore the objects of small size
+    for object in objects:
+        for x, y in object:
+            object_img[y,x] = 255
 
     for object in objects:
         x, y = object[0]
@@ -88,6 +98,8 @@ for img_idx, name in enumerate(names):
             accxx.append(accxx[-1]+x*x)
             accyy.append(accyy[-1]+y*y)
             accxy.append(accxy[-1]+x*y)
+
+        # Fit the tangent lines of every pixel on the contour
         ks, kks, angles = [], [], []
         for i in range(len(object)):
             sumx = accx[(i+hs)%llen] - accx[i-hs-1]
@@ -101,6 +113,8 @@ for img_idx, name in enumerate(names):
             sumyy = sumyy if sumyy >= 0 else sumyy+accyy[-1]
             sumxy = sumxy if sumxy >= 0 else sumxy+accxy[-1]
             denox, denoy = n*sumxx - sumx*sumx, n*sumyy - sumy*sumy
+
+            # k and kk represent the slope of the tangent line and the normal line respectively
             if denox > denoy:
                 k = (n * sumxy - sumx * sumy) / denox if denox != 0 else 1000
             else:
@@ -109,36 +123,25 @@ for img_idx, name in enumerate(names):
             if abs(kk) < 1:
                 b = sumy/n - kk*sumx/n
                 ex = int(sumx/n+0.5)
-                '''
-                for x in range(ex-rad, ex+rad):
-                    y = int(kk*x + b + 0.5)
-                    xx, yy = x+w//2, y+h//2
-                    if xx >= 0 and xx < w*2 and yy>=0 and yy < h*2:
-                        None
-                '''
             else:
                 kk = 1/kk
                 b = sumx/n - kk*sumy/n
                 ey = int(sumy/n+0.5)
-                '''
-                for y in range(ey-rad, ey+rad):
-                    x = int(kk*y + b+0.5)
-                    xx, yy = x+w//2, y+h//2
-                    if xx >= 0 and xx < w*2 and yy>=0 and yy < h*2:
-                        None
-                '''
             angle = math.atan(k)*180/np.pi
             ks.append(k)
             kks.append(kk)
             angles.append(angle)
+
+        # an abrupt turning in direction means a large value in curvature, these points will be ignored
         arc_pixs = np.zeros(len(object))
         for i in range(len(object)):
             pre_i, rear_i = i-7, (i+7)%llen
             delta_angle = angles[rear_i] - angles[pre_i]
             delta_angle = min(abs(delta_angle), abs(delta_angle+180), abs(delta_angle-180))
             x, y = object[i]
-            #cont_img[y,x] = 255 if delta_angle < 25 else 80
             arc_pixs[i] = 1 if delta_angle < 25 else 0
+
+        #find all the arcs of an object
         arcs = []
         for i in range(len(object)):
             if arc_pixs[i] == 1 and arc_pixs[i-1] == 0:
@@ -148,17 +151,25 @@ for img_idx, name in enumerate(names):
                         arcs.append(arc)
                         i = j+1
                         break
-        arcs = [arc for arc in arcs if len(arc)>80]
+        arcs = [arc for arc in arcs if len(arc)>80] # only keep the long arcs
         for arc in arcs:
             for x, y in arc:
-                cont_img[y, x] = 80
+                arcs_img[y, x] = 255
 
+        # For every arc, fit an ellipse,
         ellipses = []
         for arc in arcs:
             params = lsqEllipse(arc)
             if params is None:
                 continue
             xc, yc, a, b, theta = params
+            # for showing
+            coors = ellipseCoordits(xc, yc, a, b, theta, 1000)
+            for x, y in coors:
+                x, y = int(x + 0.5), int(y + 0.5)
+                if x >= 0 and x < w and y >= 0 and y < h:
+                    ellipses_img[y, x] = 255
+
             if a < 30 or b < 30 or a > 700 or b > 700:
                 continue
             if a/b > 1.5 or b/a > 1.5:
@@ -174,13 +185,17 @@ for img_idx, name in enumerate(names):
         for x, y in coors:
             x, y = int(x + 0.5), int(y + 0.5)
             if x >= 0 and x < w and y >= 0 and y < h:
-                cont_img[y, x] = 255
+                cirs_img[y, x] = 255
                 img[y,x] = np.array(np.zeros(3).astype(np.uint8))
 
     print(img_idx, ': ', name)
     cv2.imshow('org', img)
     cv2.imshow('mask', mask)
     cv2.imshow('cont', cont_img)
+    cv2.imshow('object', object_img)
+    cv2.imshow('arcs', arcs_img)
+    cv2.imshow('ellipses', ellipses_img)
+    cv2.imshow('circles', cirs_img)
     #cv2.imshow('cross', cross_img)
     #cv2.imshow('center', cent_img)
-    cv2.waitKey(800)
+    cv2.waitKey(8000)
